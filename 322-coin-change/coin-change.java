46943c8898104df1c4eb758fd8075f48803d2db7
Divyanshu Prajapati <divyanshuprajapati111@gmail.com>
2025-12-26 14:30:32 +0530

    Time: 41 ms (9.33%) | Memory: 48 MB (5.26%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -4,17 +4,17 @@ class Solution {
         for(long x[]:dp){
             Arrays.fill(x,-1);
         }
-       int ans=(int)countCoin(0,arr,amount,dp);
+       int ans=(int)countCoin(arr.length-1,arr,amount,dp);
        if(ans==Integer.MAX_VALUE)return -1;
        return ans;
     }
     public long countCoin(int i,int[]arr,int amount,long[][]dp){
-        if(i==arr.length){
+        if(i==-1){
             if(amount==0)return 0;
             else return Integer.MAX_VALUE;
         };
         if(dp[i][amount]!=-1)return dp[i][amount];
-        long skip=countCoin(i+1,arr,amount,dp);
+        long skip=countCoin(i-1,arr,amount,dp);
         if(amount-arr[i]<0)return skip;
         long take= 1 + countCoin(i,arr,amount-arr[i],dp);
         return dp[i][amount]= Math.min(skip,take);
