00e1b714fde58dbbc5a9f0ea39047ddcad3fb2c1
Divyanshu Prajapati <divyanshuprajapati111@gmail.com>
2025-12-26 15:00:28 +0530

    Time: 25 ms (31.64%) | Memory: 47.4 MB (10.76%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,23 +1,26 @@
 class Solution {
     public int coinChange(int[] arr, int amount) {
         long dp[][]=new long[arr.length][amount+1];
-        for(long x[]:dp){
-            Arrays.fill(x,-1);
+        for(int i=0;i<dp.length;i++){
+            for(int j=0;j<dp[0].length;j++){
+                long skip=(i>0)?dp[i-1][j]:(j==0)?0:Integer.MAX_VALUE;
+              if(j-arr[i]<0) dp[i][j]=skip;
+               else{
+                 long take= 1 + dp[i][j-arr[i]]; 
+                  dp[i][j]= Math.min(skip,take);
+               }
+            }
         }
-       int ans=(int)countCoin(arr.length-1,arr,amount,dp);
+       int ans=(int)dp[arr.length-1][amount];
        if(ans==Integer.MAX_VALUE)return -1;
        return ans;
     }
-    public long countCoin(int i,int[]arr,int amount,long[][]dp){
-        if(i==-1){
-            if(amount==0)return 0;
-            else return Integer.MAX_VALUE;
-        };
-        if(dp[i][amount]!=-1)return dp[i][amount];
-        long skip=countCoin(i-1,arr,amount,dp);
-        if(amount-arr[i]<0)return skip;
-        long take= 1 + countCoin(i,arr,amount-arr[i],dp);
-        return dp[i][amount]= Math.min(skip,take);
+    // public long countCoin(int i,int[]arr,int amount,long[][]dp){
+    //     if(i==-1){
+    //         if(amount==0)return 0;
+    //         else return Integer.MAX_VALUE;
+    //     };
         
-    }
+        
+    // }
 }
