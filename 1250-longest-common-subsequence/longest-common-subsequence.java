b01d1883de9eb20ff2e888c1f3b8a55edb1d0c4d
Divyanshu Prajapati <divyanshuprajapati111@gmail.com>
2025-12-28 11:53:46 +0530

    Time: 28 ms (20.50%) | Memory: 54 MB (28.92%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,29 +1,19 @@
 class Solution {
-    static int [][]dp;
-    public int lcs(int i,int j,StringBuilder a,StringBuilder b){
-        if(i<0 || j<0)return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
-        if(a.charAt(i)==b.charAt(j)){
-            return dp[i][j]=1+lcs(i-1,j-1,a,b);
-        }
-        else{
-            return dp[i][j]=Math.max(lcs(i-1,j,a,b),lcs(i,j-1,a,b));
-        }
-
-    }
-    public int longestCommonSubsequence(String text1, String text2) {
-        //string pass by value jati hai to memory me har bar naya string create hoga so memory exceed ho jayega
-        //we use StringBUilder -> it pass by references 
-       StringBuilder a=new StringBuilder(text1);
-       StringBuilder b=new StringBuilder(text2);
+    public int longestCommonSubsequence(String a, String b) {
        int m=a.length();
        int n=b.length();
-       //i goes to m-1 to 0
-       //j goes to n-1 to 0
-       dp=new int[m][n];
-       for(int x[]:dp)Arrays.fill(x,-1);
-       return lcs(m-1,n-1,a,b);
-
-
+       //i goes to m-1 to 0 //j goes to n-1 to 0
+      int[][] dp=new int[m][n];
+      for(int i=0;i<dp.length;i++){
+        for(int j=0;j<dp[0].length;j++){
+            int p=(i>=1 && j>=1)? dp[i-1][j-1]:0;
+            int q=(i>=1)?dp[i-1][j]:0;
+            int r=(j>=1)?dp[i][j-1]:0;
+            if(a.charAt(i)==b.charAt(j)) dp[i][j]=1+p;
+            else dp[i][j]=Math.max(q,r);
+        
+        }
+      }
+      return dp[m-1][n-1];
     }
 }
