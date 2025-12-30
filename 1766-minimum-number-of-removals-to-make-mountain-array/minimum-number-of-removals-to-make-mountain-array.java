ef5058ccbedc525a7c8f995b1c312fa1cdcb2ba5
Divyanshu Prajapati <divyanshuprajapati111@gmail.com>
2025-12-30 14:45:18 +0530

    Time: 41 ms (34.18%) | Memory: 46.8 MB (14.56%) - LeetSync

---
+++ 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int minimumMountainRemovals(int[] nums) {
+        int n=nums.length;
+        int []dp1=new int[n];
+        int []dp2=new int[n];
+        //fill dp1 increasing subsequence-->
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[j]<nums[i]){
+                    dp1[i]=Math.max(dp1[i],dp1[j]);
+                }
+            }
+            dp1[i]+=1;
+        }
+        //for decreasing subsequence -->
+        for(int i=n-1;i>=0;i--){
+            for(int j=n-1;j>i;j--){
+                if(nums[j]<nums[i]){
+                    dp2[i]=Math.max(dp2[i],dp2[j]);
+                }
+            }
+            dp2[i]+=1;
+        }
+        int max=0;
+        for(int i=0;i<n;i++){
+            //dp[i]>1 -> measn agar 1 se greater nhi hoga to mountain nahi banega
+            if(dp1[i]>1 && dp2[i]>1){
+                 max=Math.max(max,dp1[i] + dp2[i]-1);
+            }
+          
+        }
+        //means koi mountain nahi ban raha hai 
+        if(max<3)return 0;
+        else return n-max;
+    }
+}
