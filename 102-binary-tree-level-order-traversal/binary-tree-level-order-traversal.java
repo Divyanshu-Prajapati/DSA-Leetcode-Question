ae698c56d059da9f2fe21367db43058925e185ec
Divyanshu Prajapati <divyanshuprajapati111@gmail.com>
2026-01-05 22:37:21 +0530

    Time: 2 ms (22.85%) | Memory: 46.8 MB (61.76%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -5,21 +5,41 @@ class Solution {
         if(root==null)return 0;
         return 1 + Math.max(level(root.left),level(root.right));
     }
-    public void print(TreeNode root,int level,List<Integer> temp){
-        if(root==null)return;
-        if(level==n)temp.add(root.val);
-        print(root.left,level+1,temp);
-        print(root.right,level+1,temp);
+    public static class Pair{
+        TreeNode node;
+        int level;
+        Pair(TreeNode node,int level){
+            this.node=node;
+            this.level=level;
+        }
+    }
+
+    private void BFS(TreeNode root,List<List<Integer>> ans){
+        Queue<Pair> q=new LinkedList<>();
+        if(root!=null)q.add(new Pair(root,0));
+        while(q.size()>0){
+            Pair front=q.remove();
+            TreeNode temp=front.node;
+            int level=front.level;
+
+            ans.get(level).add(temp.val);
+
+            if(temp.left!=null){
+                q.add(new Pair(temp.left,level+1));
+                }
+            if(temp.right!=null){
+                q.add(new Pair(temp.right,level+1));
+            }
+        }
     }
     public List<List<Integer>> levelOrder(TreeNode root) {
         int l=level(root);
         List<List<Integer>> ans=new ArrayList<>();
         for(int i=0;i<l;i++){
-            n=i;
             List<Integer> temp=new ArrayList<>();
-            print(root,0,temp);
             ans.add(temp);
         }
+        BFS(root,ans);
         return ans;
     }
 }
