7e153fa27a4e859c6dc223e6b1b581e4a255242b
Divyanshu Prajapati <divyanshuprajapati111@gmail.com>
2026-01-19 20:19:11 +0530

    Time: 3 ms (24.56%) | Memory: 46.6 MB (8.62%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.java
+++ 871-keys-and-rooms/keys-and-rooms.java
@@ -4,14 +4,14 @@ class Solution {
         boolean[] isvis=new boolean[n];
         Queue<Integer> q=new LinkedList<>();
         for(int x:rooms.get(0)){
-            q.offer(x);
+            q.add(x);
         }
         isvis[0]=true;
         while(q.size()>0){
             int front=q.remove();
             if(isvis[front]!=true){
                for(int x:rooms.get(front)){
-                q.offer(x);
+                q.add(x);
                 }
             }
             isvis[front]=true;  
