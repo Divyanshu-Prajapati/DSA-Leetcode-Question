43b173c6dfdceeeedf280d7bed7e2099b16b082e
Divyanshu Prajapati <divyanshuprajapati111@gmail.com>
2026-02-04 01:11:55 +0530

    Time: 1 ms (87.37%) | Memory: 47.6 MB (33.68%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,27 +1,26 @@
 class Solution {
+    //by DFS -> Depth First Search
     public int findCircleNum(int[][] adj) {
         int n=adj.length;
         int ans=0;
         boolean[] isvisited=new boolean[n];
         for(int i=0;i<n;i++){
             if(!isvisited[i]){
+                dfs(i,adj,isvisited);
                 ans++;
-                bfs(i,adj,isvisited);
+                
             }
         }
         return ans;
     }
-    public void bfs(int node,int[][] adj,boolean[]isvisited){
-        Queue<Integer> q=new LinkedList<>();
-        q.add(node);
-        while(q.size()>0){
-            int temp=q.remove();
-            for(int i=0;i<adj.length;i++){
-                if(adj[temp][i]==1 && isvisited[i]==false){
-                    q.add(i);
-                    isvisited[i]=true;
+    public void dfs(int i,int[][] adj,boolean[]isvisited){
+             isvisited[i]=true;
+            for(int j=0;j<adj.length;j++){
+                if(adj[i][j]==1 && isvisited[j]==false){
+                   dfs(j,adj,isvisited);
+                    
                 }
             }
-        }
+        
     }
 }
