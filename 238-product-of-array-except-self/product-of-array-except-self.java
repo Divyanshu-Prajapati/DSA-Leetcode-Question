a35351a098a3e7897b02a44d028c2cbcfb8739c7
Divyanshu Prajapati <divyanshuprajapati111@gmail.com>
2026-02-03 15:22:04 +0530

    Time: 2 ms (94.29%) | Memory: 72.3 MB (24.67%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,23 +1,44 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n=nums.length;
-        int ans[]=new int[n];
-        int[] prefixproduct=new int[n];
-        int []suffixproduct=new int[n];
-
-        //for prefix
-        prefixproduct[0]=1;
-        for(int i=1;i<n;i++){
-            prefixproduct[i]=prefixproduct[i-1]*nums[i-1];
-        }
-        suffixproduct[n-1]=1;
-        for(int i=n-2;i>=0;i--){
-            suffixproduct[i]=suffixproduct[i+1]*nums[i+1];
-        }
+        int arr[]=new int[n];
+        int product=1;
+        //find the product
         for(int i=0;i<n;i++){
-            ans[i]=prefixproduct[i]*suffixproduct[i];
+            if(nums[i]==0){
+                continue;
+            }
+            product=product*nums[i];
         }
-        return ans;
-    }
+        //now count 
+        int zero=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==0){
+                zero++;
+            }
+        }
+        if(zero==1){
+            for(int i=0;i<n;i++){
+                if(nums[i]==0){
+                    arr[i]=product;
+                }
+                else{
+                    arr[i]=0;
+                }
+            }
+        }
+        else if(zero<1){
+            for(int i=0;i<n;i++){
+                arr[i]=product/nums[i];
+            }
+            
+        }
+        else{
+            for(int i=0;i<n;i++){
+                arr[i]=0;
+            }
+        }
+        return arr;
 
+    }
 }
