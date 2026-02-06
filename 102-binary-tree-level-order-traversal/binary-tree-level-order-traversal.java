73a894994e3ca9aab93a71ee035afaeebde71831
Divyanshu Prajapati <divyanshuprajapati111@gmail.com>
2026-02-06 11:39:29 +0530

    Time: 1 ms (95.49%) | Memory: 47.1 MB (18.68%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -1,45 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public int n;
-
-    public int level(TreeNode root){
-        if(root==null)return 0;
-        return 1 + Math.max(level(root.left),level(root.right));
-    }
-    public static class Pair{
-        TreeNode node;
-        int level;
-        Pair(TreeNode node,int level){
-            this.node=node;
-            this.level=level;
-        }
-    }
-
-    private void BFS(TreeNode root,List<List<Integer>> ans){
-        Queue<Pair> q=new LinkedList<>();
-        if(root!=null)q.add(new Pair(root,0));
-        while(q.size()>0){
-            Pair front=q.remove();
-            TreeNode temp=front.node;
-            int level=front.level;
-
-            ans.get(level).add(temp.val);
-
-            if(temp.left!=null){
-                q.add(new Pair(temp.left,level+1));
-                }
-            if(temp.right!=null){
-                q.add(new Pair(temp.right,level+1));
-            }
-        }
-    }
     public List<List<Integer>> levelOrder(TreeNode root) {
-        int l=level(root);
-        List<List<Integer>> ans=new ArrayList<>();
-        for(int i=0;i<l;i++){
+        List<List<Integer>> list=new ArrayList<>();
+        Queue<TreeNode> q=new LinkedList<>();
+        if(root!=null)q.add(root);
+        while(!q.isEmpty()){
             List<Integer> temp=new ArrayList<>();
-            ans.add(temp);
+            int size=q.size();
+            while(size!=0){
+                TreeNode value=q.poll();
+                temp.add(value.val);
+                if(value.left!=null)q.add(value.left);
+                if(value.right!=null)q.add(value.right);
+                size--;
+            }
+            list.add(temp);
         }
-        BFS(root,ans);
-        return ans;
+        return list;
     }
 }
